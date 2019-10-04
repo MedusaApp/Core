397270f1e28117e15fe1adaabe346d0aa6f33f16
Timberwoof <gitwoof@timberwoof.com>
2019-10-04 16:36:12 -0700

    installed Dusk and added one example test

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('http://127.0.0.1:8000/login')
+                    ->assertSee('MEDUSA');
+        });
+    }
+}
