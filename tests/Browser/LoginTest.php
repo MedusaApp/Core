33be570d7a567134e32336bdf39016478f862050
Timberwoof <gitwoof@timberwoof.com>
2019-10-04 17:09:23 -0700

    first login test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -18,6 +18,19 @@ class LoginTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('http://127.0.0.1:8000/login')
                     ->assertSee('MEDUSA');
+            
+            $browser->assertSee('This site uses cookies to personalize content, to provide social media features and to analyze traffic.');
+            $browser->press('OK');
+            $browser->assertDontSee('This site uses cookies to personalize content, to provide social media features and to analyze traffic.');
+            
+            $browser->assertSee('E-Mail Address');
+            $browser->assertSee('Password');
+            
+            $browser->type('email', 'noOne@trmn.org');
+            $browser->type('password', 'nonsense');
+            $browser->press('Login');
+            $browser->assertSee('These credentials do not match our records.');
+            
         });
     }
 }
