974a229ea0c568309d5c74e8069ed704c2d0bfc6
David Weiner <david.l.weiner30030@gmail.com>
2019-10-14 20:58:49 -0400

    Use local fontawesome instead of CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,11 +14,6 @@
         src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
         crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-            crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/899e39c2d7.js" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
     <!-- Additional Javascript files to include -->
 @stack('js')
 
